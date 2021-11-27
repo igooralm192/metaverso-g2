68c61f435c0003f2ce4352848a3a8fef56f66314
Gustavo Quinteiro <gustavo.quinteiro@ufba.br>
2021-11-27 13:07:45 -0300

    Consertando movimento da lua

--- CodigoBase/estrutura-solar.js
+++ CodigoBase/estrutura-solar.js
@@ -7,10 +7,15 @@ var renderer;
 var scene;
 var camera;
 var cameraControl;
+var speedPause = 1
+var speedFactor = 0.005 * speedPause
+var distanceFactor = 0.005
 
-var terraData = getPlanetData(365.2564, 0.024, 1500*2, "Terra", "../resources/Textures/earthmap1k.jpg", 100, 48);
-var solData = getPlanetData(0, 0.0009, 0, "Sol", "../resources/Images/sunmap.jpg", 500, 48);
-var luaData = getPlanetData(29.7, 0.052, 1000/3.85, "Lua", "../resources/Textures/moonmap1k.jpg", 30, 48 )
+var terraData = getPlanetData(0 * speedFactor, 1 * speedFactor, 200*2, "Terra", "../resources/Textures/earthmap1k.jpg", 100, 48);
+
+var solData = getPlanetData((1/365.2564) * speedFactor, 0.0009 * speedFactor, 1496000*distanceFactor, "Sol", "../resources/Images/sunmap.jpg", 500, 48);
+
+var luaData = getPlanetData(-(1/29.7) * speedFactor, (1/29.7) * speedFactor, 384400*distanceFactor, "Lua", "../resources/Textures/moonmap1k.jpg", 50, 48)
 
 var planets = [];
 
@@ -34,51 +39,58 @@ function main() {
 	renderer.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.getElementById("WebGL-output").appendChild(renderer.domElement);
+	const ambLight 			= new THREE.AmbientLight( 0xFFFFFF, 0.25 ); 
+	ambLight.name 			= "ambLight";
+	ambLight.visible 		= true;
+	scene.add(ambLight)
 
 	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100000);
-
-	camera.position.x = -480;
-	camera.position.y = 4800;
-	camera.position.z = 3200;
-	camera.lookAt(scene.position);
-
 	cameraControl = new OrbitControls(camera, renderer.domElement);
 	loadMesh(solData);
 	loadMesh(terraData);
+	loadMesh(luaData)
 	update(cameraControl);
+	var terra = scene.getObjectByName("Terra");
+	moveCamera(terra)
+	camera.lookAt(scene.position);
 }
 
 function update(cameraControl) {
-    cameraControl.update();
+	cameraControl.update();
 
     var time = Date.now();
 	planets.forEach(element => {
 		movePlanet(element, time);
 	});
-	var lua = scene.getObjectByName("Lua");
-    moveMoon(lua, luaData, time);
     renderer.render(scene, camera);
     requestAnimationFrame(function () {
         update(cameraControl);
     });
 }
 
-function movePlanet(myPlanet, myTime) {
-	myPlanet[0].rotateY(myPlanet[1].rotationRate);
-	if(myPlanet[0].name != 'Sol') {
-		myPlanet[0].position.x = Math.cos(myTime 
-			* (1.0 / (myPlanet[1].orbitRate * 200)) + 10.0) 
-			* myPlanet[1].distanceFromAxis;
-		myPlanet[0].position.z = Math.sin(myTime 
-			* (1.0 / (myPlanet[1].orbitRate * 200)) + 10.0) 
-			* myPlanet[1].distanceFromAxis;
-	}	
-}
+function moveCamera(terra) {
+	camera.position.x = terra.position.x + 0;
+	camera.position.y = terra.position.y + 50;
+	camera.position.z = terra.position.z + 900;
+	camera.lookAt(terra.position);
+  }
 
-function moveMoon(satelite, sateliteData, myTime){
-	satelite.position.x = Math.cos(myTime * (1.0/(sateliteData.orbitRate * 15 )) + 15.0) * sateliteData.distanceFromAxis
-	satelite.position.z = Math.sin(myTime * (1.0/(sateliteData.orbitRate * 15 )) + 15.0) * sateliteData.distanceFromAxis
-	angleMoonEarth();
+function movePlanet(myPlanet, myTime) {
+	if(myPlanet[0].name == 'Sol') {
+
+	}else if (myPlanet[0].name == 'Lua'){
+		myPlanet[0].lookAt(new THREE.Vector3(0,0,0))
+		myPlanet[0].position.x = Math.cos(myTime 
+			* ((myPlanet[1].orbitRate ))) 
+			* myPlanet[1].distanceFromAxis;
+
+		myPlanet[0].position.z = Math.sin(myTime 
+			* ((myPlanet[1].orbitRate )) ) 
+			* myPlanet[1].distanceFromAxis;
+	}else{
+		myPlanet[0].rotateY(myPlanet[1].rotationRate);
+	}
+
 }
 
 function loadMesh(planetData) {
@@ -96,21 +108,23 @@ function loadMesh(planetData) {
             lightMap: texture,
             transparent: true,
             opacity: 0.8,
-            shading: THREE.SmoothShading
+            flatShading: THREE.SmoothShading
 		});
 		texture.wrapS = THREE.ClampToEdgeWrapping;
     	texture.wrapT = THREE.ClampToEdgeWrapping;
     	texture.minFilter = THREE.NearestFilter;
 	}
-	else {
-		material = new THREE.MeshPhongMaterial({	
+	else if (planetData.name == "Terra"){
+		material = new THREE.MeshLambertMaterial({	
 			map: texture
 		});
+	} else{
+		material = new THREE.MeshPhongMaterial({map: texture})
 	}
 
 	const geometry = new THREE.SphereGeometry( planetData.size, planetData.size, planetData.size );
 	var planet = new THREE.Mesh( geometry, material );
-	planet.position.set(planetData.distanceFromAxis, 0, 0);
+	
 	planet.castShadow = false;
 	planet.name = planetData.name;
 
@@ -118,7 +132,6 @@ function loadMesh(planetData) {
 		let spriteMaterial = new THREE.SpriteMaterial(
 			{
 				map: new THREE.ImageUtils.loadTexture("../resources/Images/glow.png"),
-				useScreenCoordinates: false,
 				color: 0xffffee,
 				transparent: true,
 				blending: THREE.AdditiveBlending
@@ -126,19 +139,21 @@ function loadMesh(planetData) {
 		const sunLight = new THREE.PointLight(0xffdcb4, 2.0);
 		let sprite = new THREE.Sprite(spriteMaterial);
 		sprite.scale.set(1800, 1800, 1.0);
+		planet.position.set(planetData.distanceFromAxis, 0, 0);
 		planet.add(sunLight);
 		planet.add(sprite);
-	} else{
-		console.log(luaData.distanceFromAxis);
-		texture = textureLoader.load(luaData.texture, function ( texture ) {
-			texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-		});
-		var moon = new THREE.Mesh(new THREE.SphereGeometry(luaData.size, luaData.size, luaData.size), new THREE.MeshPhongMaterial({map: texture}));
-		moon.position.set(luaData.distanceFromAxis, 22.741, planet.position.z);
-		moon.name = luaData.name;
-		
-		planet.add(moon);
+	} else if (planet.name == "Terra"){
+		planet.position.set(0, 0, 0);
+		planet.rotation.set(0, 0.453786, 0)
+		const axis = new THREE.AxesHelper(1000 );
+		planet.add(axis)
 		console.log(planet);
+	}else if (planet.name == "Lua"){
+		var terra = scene.getObjectByName("Terra");
+		planet.position.set(luaData.distanceFromAxis,0, 0);
+		planet.name = luaData.name;
+		const axii = new THREE.AxesHelper(1000);
+		planet.add(axii)
 	}
 	var data = [planet, planetData];
 	planets.push(data);
@@ -160,6 +175,15 @@ function angleMoonEarth(){
 			lua.position.z * terra.position.z) 
 		/ (Math.sqrt(terrap) * Math.sqrt(luap)));
  	//console.log(a);
+}
+
+function pauseRotation(){
+	if (speedPause == 1){
+		speedPause = 0;
+	}
+	else{
+		speedPause = 1;
+	}
 }
 
 main();
